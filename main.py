e1247f00880b5142d73115fba6b44b0885558004
survivor <pbb_amir@hotmail.com>
2024-03-19 17:57:28 +0500

    Workout needed but agent working

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import chainlit as cl
 from chainlit.input_widget import TextInput
 from modules.data_process import if_exists, return_data
+from modules.agents import Agents
 
 
 @cl.on_chat_start
 async def main():
     try:
+        agent = Agents()
         name = await cl.AskUserMessage(content="Please Enter your Name", timeout=60).send()
         name = name['output']
         if name and if_exists(str(name)):
-            print(f"Data is {return_data(str(name))}")
+            data = return_data(str(name))
+            print(f"Data is {return_data(str(name))[0]}")
+
+            res = agent.opener.invoke({'input': f"{data[0]}"}, return_only_outputs=True)
+            print(res)
         else:
             await cl.Message(content="No name exists.").send()
     except Exception as e:
--- modules/prompt.py
+++ modules/prompt.py
@@ -1,8 +1,27 @@
-OPENER_PREFIX = """
-Generate the following conversations as best you can.
-You have been provided with the best
+OPENER = """
+You are a sales representative Named Robo for a software consultancy firm Antematter. Your goal is to generate personalized cold emails to 
+potential leads, inquiring about their needs and convincing them to choose your firm as their software consultancy provider.
+
+For each lead, you will be provided with their name, company name, and industry.
+Use this information to personalize the email for them.
+The email should follow these guidelines:
+    1.Subject line: Craft a brief subject line that's less than 30 characters and grabs the reader's attention.
+    2.Body:   The email body should be no longer than three paragraphs.
+            Paragraph 1: Introduce yourself and your company, and express interest in understanding their software needs.
+            Paragraph 2: Inquire about the scope and budget they have in mind for their project.
+            Paragraph 3: Highlight your firm's expertise and ability to deliver quality software solutions,
+     without using flowery or flattering language.
+    3.Tone: Maintain a human and conversational tone throughout the email, avoiding mechanical or robotic language.
+    4.Closing: End the email with a call-to-action, such as scheduling a meeting or a phone call to discuss further.
+Your task is to generate a personalized cold email for each lead, following the guidelines provided.
+Your tools are empty so create the response from data provided:
+{tools}
+[{tool_names}]
+You will be provided with data of client:
+Data:{input}
+{agent_scratchpad}
 """
 
-OPENER_INSTRUCTIONS = """
 
+ESCALATOR = """
 """
